9f1beef2600ce76043a512794cac3fc79e694e1c
LuanGDantas <luan.dantas123@hotmail.com>
2021-06-24 17:40:53 -0300

    💾 🛠Creation and / or configuration of the Database (sqlite) with the user table, TYPEORM, das Migrations, da user entity, of server, router and controller of user creation

---
+++ src/entities/User.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid"
+
+@Entity("users")
+class User {
+
+  @PrimaryColumn()
+  //readonly apenas leitura; inserção apenas pela própria entidade 0
+  //yarn add uuid e @types/uuid -D
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
